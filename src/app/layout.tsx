dee73773d3578aefcac7cb71d3eee0a8021ecba5
Allen7702 <allen22052000@gmail.com>
2025-07-15 16:51:26 +0300

    feat: wrap layout in ThemeProvider to manage theme context

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import '../styles/globals.css';
 import AuthWrappedLayout from '@/components/ClientLayout';
+import { ThemeProvider } from '@/context/ThemeContext';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -14,7 +15,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body className={inter.className}>
-        <AuthWrappedLayout>{children}</AuthWrappedLayout>
+        <ThemeProvider>
+          <AuthWrappedLayout>{children}</AuthWrappedLayout>
+        </ThemeProvider>
       </body>
     </html>
   );

69cac01897205364eae6b84ebc552c7653c045d2
Allen7702 <allen22052000@gmail.com>
2025-07-15 16:50:55 +0300

    feat: replace SideNav with AppSidebar and wrap layout in SidebarProvider

--- src/components/ClientLayout.tsx
+++ src/components/ClientLayout.tsx
@@ -2,11 +2,12 @@
 
 import { usePathname } from 'next/navigation';
 import { ToastContainer } from 'react-toastify';
-import SideNav from './SideNav/SideNav';
 import { AuthProvider, useAuth } from '@/context/AuthContext';
 import { setupAxiosInterceptors } from '@/services/api';
 import LoginPage from '@/app/login/page';
 import { useEffect } from 'react';
+import AppSidebar from '@/layout/AppSidebar';
+import { SidebarProvider } from '@/context/SidebarContext';
 
 const ClientLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const pathname = usePathname();
@@ -16,7 +17,6 @@ const ClientLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => 
     setupAxiosInterceptors(token, handleRefreshToken);
   }, [token, handleRefreshToken]);
 
-  // Show LoginPage without sidebar for /login route or if not authenticated
   if (pathname === '/login' || !user) {
     return (
       <div className="min-h-screen bg-gray-100 flex items-center justify-center text-black">
@@ -25,12 +25,13 @@ const ClientLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => 
     );
   }
 
-  // Show sidebar and content for authenticated routes
   return (
     <div className="flex text-black bg-gray-100 h-full">
-      <SideNav />
-      <div className="ml-64 flex-1 p-6 flex-grow bg-gray-100">{children}</div>
-      <ToastContainer position="top-right" autoClose={3000} />
+      <SidebarProvider>
+        <AppSidebar />
+        <div className="ml-64 flex-1 p-6 flex-grow bg-gray-100">{children}</div>
+        <ToastContainer position="top-right" autoClose={3000} />
+      </SidebarProvider>
     </div>
   );
 };
